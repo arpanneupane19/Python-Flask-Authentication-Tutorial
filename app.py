593676c246dbca76ef5949caec57fe1d1a86b18a
Isuru Dissanayake <98075833+IsuruDilanga@users.noreply.github.com>
2023-03-01 22:20:13 +0530

    change "db = SQLAlchemy(app)" location
    I change the "db = SQLAlchemy(app)" line location and took it 1 row below otherwise it coming error of this "RuntimeError( RuntimeError: Either 'SQLALCHEMY_DATABASE_URI' or 'SQLALCHEMY_BINDS' must be set [duplicate]"

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
+
 bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'thisisasecretkey'
 
 
